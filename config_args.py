e16a08e43dd1ff3ddd6bc6f27e3666024f69427c
coolbit <coolbit.in@gmail.com>
2013-09-03 01:16:29 +0800

    增加rsync --timeout 参数

--- config_args.py
+++ config_args.py
@@ -25,7 +25,7 @@ MAX_BUSY_NUM = 3
 #最小等待间隔(s)
 MIN_WAITING_TIME = 30
 
-UBUNTU_ARGS = 'rsync -6 -av --delete-after --ignore-errors --force -h' \
+UBUNTU_ARGS = 'rsync -6 -av --delete-after --ignore-errors --timeout=3600 --force -h' \
               + ' --exclude *ia64*' \
               + ' --exclude *powerpc*' \
               + ' --exclude *sparc*' \
@@ -44,15 +44,15 @@ UBUNTU_ARGS = 'rsync -6 -av --delete-after --ignore-errors --force -h' \
               + ' rsync://mirrors6.ustc.edu.cn/ubuntu' \
               + ' ' + MIRROR_ADDR + 'ubuntu'
 
-DEEPIN_ARGS = 'rsync -6 -av --delete-after --ignore-errors --force -h' \
+DEEPIN_ARGS = 'rsync -6 -av --delete-after --ignore-errors --timeout=3600 --force -h' \
               + ' rsync://mirrors6.ustc.edu.cn/deepin' \
               + ' ' + MIRROR_ADDR + 'deepin'
 
-GENTOO_ARGS = 'rsync -6 -av --delete-after --ignore-errors --force -h' \
+GENTOO_ARGS = 'rsync -6 -av --delete-after --ignore-errors --timeout=3600 --force -h' \
               + ' rsync://mirrors6.ustc.edu.cn/gentoo' \
               + ' ' + MIRROR_ADDR + 'gentoo'
 
-QOMO_ARGS   = 'rsync -6 -av --delete-after --ignore-errors --force -h' \
+QOMO_ARGS   = 'rsync -6 -av --delete-after --ignore-errors --timeout=3600 --force -h' \
               + ' --exclude *.iso' \
               + ' --exclude *.iso*' \
               + ' rsync://mirrors6.ustc.edu.cn/qomo' \
