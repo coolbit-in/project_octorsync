81d0ddc4d766f24893c84fbaef2c46e198d41f5b
coolbit <coolbit.in@gmail.com>
2013-11-18 18:12:05 +0800

    add file catch_info.py

---
+++ catch_info.py
@@ -0,0 +1,15 @@
+import re
+import os
+
+def catch_info(file_path):
+    with open(file_path, 'r') as file_handle:
+        re_pattern= r'total size is (\S+)'
+        catch_list = re.findall(re_pattern, file_handle.read())
+        if not len(catch_list) == 0:
+            return catch_list[len(catch_list) - 1]
+        else:
+            return None
+
+if __name__ == '__main__':
+    file_path = os.path.join(os.getcwd(), 'logs', 'deepin.log')
+    print catch_info(file_path)
